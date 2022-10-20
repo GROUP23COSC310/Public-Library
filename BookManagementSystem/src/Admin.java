19cfe964aa30676c1cea40e21370c28f879dbec9
Nihad Alakbarzade <81528220+nihadae@users.noreply.github.com>
2022-10-19 21:02:55 -0700

    Delete BookManagementSystem directory

--- BookManagementSystem/src/Admin.java
+++
@@ -1,8 +0,0 @@
-public class Admin {
-    private final String _username = "admin";
-    private final String _password = "admin123";
-
-    public String get_password() {
-        return _password;
-    }
-}
